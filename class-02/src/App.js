51d63a2dc53b1f51a2e6e9b0e36b64b66556c495
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-04 17:15:56 +0500

    Update App.js

--- class-02/src/App.js
+++ class-02/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
  function App() {
-  // return <h1>Hello World </h1>
-  const name ="Muhammad Ahsan"
-  return name
+  return <h1>Hello World </h1>
+  // const name ="Muhammad Ahsan"
+  return
 }
 
 export default App
