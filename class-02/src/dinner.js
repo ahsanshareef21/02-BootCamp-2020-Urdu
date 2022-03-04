740c63e2457a578357b16af50bd2008aad3051ca
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-04 17:56:23 +0500

    make new file

---
+++ class-02/src/dinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dinner(){
+    return(
+        <div>
+        <h1> Eating Food </h1>
+         <h1> Eggs</h1>
+         </div>
+    )
+}
+export default Dinner;
