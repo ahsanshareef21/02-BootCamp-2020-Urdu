24afa5a45a296e7cd7d6777517f48dd4adeffaeb
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-04 17:10:25 +0500

    Update App.js

--- class-02/src/App.js
+++ class-02/src/App.js
@@ -1,12 +1,7 @@
-import react from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    <h1> Hello World</h1>
-  );
+ function App() {
+  return <h1>Hello World </h1>
 }
 
-export default App;
+export default App
