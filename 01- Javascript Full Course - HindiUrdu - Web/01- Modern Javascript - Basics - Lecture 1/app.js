742ccc61b43d7be4d17b44193216c7528eb34894
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-29 08:35:03 +0500

    Update app.js

--- 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app.js
+++ 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app.js
@@ -5,3 +5,15 @@ console.log("This is my File in Console");
 //Varibales:
 var fname= "Ahsan";
 console.log(fname);
+
+
+
+// let and const
+
+let fname= "Ali";
+console.log(fname);
+
+
+
+const fname= "Raza";
+console.log(fname);
