f13382c0e2b53db8db75de3d454e41173078d0cf
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-29 14:47:38 +0500

    Update app1.js

--- 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
+++ 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
@@ -2,5 +2,7 @@ document.write(" <h2>javascript Lecture - 02 </h2>")
 
 // Common String methods
 
-let email="Muhammadahsan7099@gmail.com";
-// Search ythe lat no from the string
+let email = "Muhammadahsan7099@gmail.com";
+// Search the lat no from the string
+let resut = email.lastIndexOf(9);
+console.log(resut);
