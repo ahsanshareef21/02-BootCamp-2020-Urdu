c67df19f566b380a65d21083b0415a807e89db57
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-30 11:58:10 +0500

    Update

--- 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
+++ 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
@@ -1,4 +1,4 @@
-document.write(" <h2>javascript Lecture - 02 </h2>")
+document.write(" <h2>javascript Lecture - 02 </h2>");
 
 // Common String methods
 
@@ -6,57 +6,45 @@ let email = "Muhammadahsan7099@gmail.com";
 
 // Search the last no from the string
 
-let name = email.lastIndexOf('m');
+let name = email.lastIndexOf("m");
 
 console.log(name);
-
 
 //slice
 // string.slice(form , to)
 
-let sliceResut = email.slice(0,6)
+let sliceResut = email.slice(0, 6);
 console.log(sliceResut);
 
+// Replace from the string
 
-// // Replace from the string
-
-let change = email.replace('ahsan','aliRaza');
+let change = email.replace("ahsan", "aliRaza");
 console.log(change);
 
-// //Substring  Startind index sy ktny no print krany han
+//Substring  Startind index sy ktny no print krany han
 
-let subStr = email.substr(5,10);
+let subStr = email.substr(5, 10);
 console.log(subStr);
 
 // Find Domain Nam with Slice Method
 
-let domainName= email.slice(email.lastIndexOf('@'), email.length);
+let domainName = email.slice(email.lastIndexOf("@"), email.length);
 console.log(domainName);
 
 // Template litrals
 
-
-
-const title='Blog Post';
-const author='Ahsan';
+const title = "Blog Post";
+const author = "Ahsan";
 const likes = 300;
 
-let no=`
+let no = `
  <h2>${title}</h2>
  <p>${author}</p>
  <span>This Blog has ${likes}</span>`;
 
 console.log(no);
 
-
 // Array Method
 
-
-
-
-
-
-
-
-
-
+let name = ["ali", "raza", "ahsan"];
+console.log(name);
