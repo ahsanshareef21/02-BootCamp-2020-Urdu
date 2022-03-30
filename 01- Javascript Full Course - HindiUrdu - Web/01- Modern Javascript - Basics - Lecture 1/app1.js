eb524f381689fec88012553e97b4b010222f6bf9
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-30 09:29:29 +0500

    Update app1.js

--- 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
+++ 01- Javascript Full Course - HindiUrdu - Web/01- Modern Javascript - Basics - Lecture 1/app1.js
@@ -3,23 +3,26 @@ document.write(" <h2>javascript Lecture - 02 </h2>")
 // Common String methods
 
 let email = "Muhammadahsan7099@gmail.com";
-// Search the lat no from the string
-let resut = email.lastIndexOf(9);
+
+// Search the last no from the string
+
+let resut = email.lastIndexOf(m);
+
 console.log(resut);
 
 
-//slice
-//string.slice(form , to)
-let sliceResut = email.slice(0,6)
-console.log(sliceResut);
+// //slice
+// //string.slice(form , to)
+// let sliceResut = email.slice(0,6)
+// console.log(sliceResut);
 
-// Replace from the string
+// // Replace from the string
 
-let replace= email.replace('ahsan','aliRaza');
-console.log(replace);
+// let replace= email.replace('ahsan','aliRaza');
+// console.log(replace);
 
-//Substring--> Main to Sub
+// //Substring--> Main to Sub
 
-let s = email.substr(5,5);
-console.log(substr);
+// let s = email.substr(5,5);
+// console.log(substr);
 
